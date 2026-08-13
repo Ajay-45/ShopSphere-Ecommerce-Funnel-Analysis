fe61e3db9f4cdf888349e9f7a8ccd18af0c89c1f
Ajay-45 <77102796+Ajay-45@users.noreply.github.com>
2026-08-14 02:05:26 +0530

    Add files via upload

---
+++ 09_geographic_performance.sql
@@ -0,0 +1,11 @@
+SELECT
+  geoNetwork.country AS country,
+  COUNT(DISTINCT fullVisitorId)AS
+unique_visitors,
+  SUM(totals.transactions)AS
+total_transactions,
+  SUM(totals.pageviews)AS
+total_pageviews
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20160801`
+GROUP BY country
+ORDER BY unique_visitors DESC;
