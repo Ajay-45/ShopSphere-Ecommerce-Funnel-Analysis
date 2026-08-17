1ef0ebcad9a16ce36e81be7b89139d491d7351d7
Ajay-45 <77102796+Ajay-45@users.noreply.github.com>
2026-08-17 23:52:51 +0530

    Delete 15_revenue_by_country.sql

--- 15_revenue_by_country.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  geoNetwork.country AS country,
-  COUNT(DISTINCT fullVisitorId) AS unique_visitors,
-  SUM(totals.transactions) AS total_transactions,
-  SUM(totals.totalTransactionRevenue) / 1000000 AS total_revenue
-FROM bigquery-public-data.google_analytics_sample.ga_sessions_20160801
-GROUP BY country
-ORDER BY total_revenue DESC;
