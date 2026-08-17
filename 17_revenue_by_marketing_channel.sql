9cf6fe462281a0c16604a1c7271071c20d53c714
Ajay-45 <77102796+Ajay-45@users.noreply.github.com>
2026-08-17 23:49:24 +0530

    Delete 17_revenue_by_marketing_channel.sql

--- 17_revenue_by_marketing_channel.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  channelGrouping,
-  COUNT(DISTINCT fullVisitorId) AS unique_visitors,
-  SUM(totals.transactions) AS total_transactions,
-  SUM(totals.totalTransactionRevenue) / 1000000 AS total_revenue
-FROM bigquery-public-data.google_analytics_sample.ga_sessions_20160801
-GROUP BY channelGrouping
-ORDER BY total_revenue DESC;
