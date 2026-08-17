12ffb596bd004157ad6f30a3c20cd160c6f520d2
Ajay-45 <77102796+Ajay-45@users.noreply.github.com>
2026-08-17 23:50:25 +0530

    Delete 12_overall_ecommerce_kpis.sql

--- 12_overall_ecommerce_kpis.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-COUNT(DISTINCT fullvisitorId) AS unique_visitors,
-SUM(totals.transactions)AS total_transactions,
-SUM(totals.pageviews)AS total_pageviews
- FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20160801` 
