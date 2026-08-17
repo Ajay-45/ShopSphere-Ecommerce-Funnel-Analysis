94f3fb16d60cd68968156b2f29d37f8a8ea776dd
Ajay-45 <77102796+Ajay-45@users.noreply.github.com>
2026-08-17 23:53:14 +0530

    Delete 16_revenue_by_device.sql

--- 16_revenue_by_device.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  device.deviceCategory,
-  COUNT(DISTINCT fullVisitorId) AS unique_visitors,
-  SUM(totals.transactions) AS total_transactions,
-  SUM(totals.totalTransactionRevenue) / 1000000 AS total_revenue
-FROM bigquery-public-data.google_analytics_sample.ga_sessions_20160801
-GROUP BY device.deviceCategory
-ORDER BY total_revenue DESC;
